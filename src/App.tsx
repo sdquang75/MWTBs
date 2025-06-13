a77051800d7589ac24de77a049ad73c944876fd9
sdquang75 <sdquang75@gmail.com>
2025-06-13 14:34:16 +0900

    6/13

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          
           <Route path="/home" element={<HomeScreen />} />
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/signup" element={<SignUpScreen />} />
