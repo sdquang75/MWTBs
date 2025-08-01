76df7f3877c4d4fa42c06601aad3fa568850708e
sdquang75 <sdquang75@gmail.com>
2025-08-01 10:35:59 +0900

    added smt before change

--- FRONTEND/my-app/src/App.tsx
+++ FRONTEND/my-app/src/App.tsx
@@ -1,4 +1,5 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import { Dashboard } from './pages/Dashboard';
 import { IngredientInput } from './pages/IngredientInput';
@@ -8,12 +9,18 @@ import { RecipeList } from './pages/RecipeList';
 import { RegisterInfo } from './pages/RegisterInfo';
 
 function App() {
-    // return <Login />;
-  //  return <RegisterInfo />;
-    //  return <Dashboard />;
-// return <IngredientInput />;
-//  return <NutritionInfo />;
-return<RecipeList/>
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register-info" element={<RegisterInfo />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/ingredient-input" element={<IngredientInput />} />
+      <Route path="/nutrition-info" element={<NutritionInfo />} />
+      <Route path="/recipe-list" element={<RecipeList />} />
+      
+      {/* Route mặc định, có thể trỏ về trang dashboard hoặc login */}
+      <Route index element={<Dashboard />} /> 
+    </Routes>
+  );
 }
-
 export default App
--- FRONTEND/my-app/src/main.tsx
+++ FRONTEND/my-app/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'; // << 1. Import
 import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* 2. Bọc App trong BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
